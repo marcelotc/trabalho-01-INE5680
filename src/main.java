fc6123ed776bdfee77a4f9294a1c5d48fb2622f7
marcelotc <marceloteixeiracortes@gmail.com>
2022-06-10 17:12:18 -0300

    renaming

--- src/main.java
+++ src/main.java
@@ -6,7 +6,7 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
-public class trabalho1 {
+public class main {
     
     public static void menu(){
         System.out.println("\tCadastro de clientes");
